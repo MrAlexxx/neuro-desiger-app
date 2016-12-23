f6c5b77df9b2ca98dd24bfec6e55e62896f2bc73
Oleksii Malitskyi <Oleksii.Malitskyi@udelphi.com>
2016-12-23 18:02:00 +0200

    added item page

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { ItemDetailComponent } from "./item-details/item-details.component";
+import { HttpModule } from "@angular/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [
+      BrowserModule,
+      HttpModule,
+      FormsModule,
+      ReactiveFormsModule
+  ],
+  declarations: [
+      AppComponent,
+      ItemDetailComponent
+  ],
+  providers:        [
+    //MyService, MyOtherService
+  ],
+  bootstrap:    [
+      AppComponent
+  ]
 })
 export class AppModule { }
---
+++ app/item-details/item-details.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'item-details',
+    templateUrl: 'item-details.component.html',
+    styleUrls: ['item-details.component.css']
+
+})
+
+export class ItemDetailComponent  {
+    name = 'Angular';
+}
+
+
